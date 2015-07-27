2aca303b37fdee72ebcc8c9b1fbbcbc36c9e4a25
Wilson G. Velandia B <willyv78@gmail.com>
2015-07-27 06:32:43 -0500

    Fin de semana 24 de julio 2015
    Fin de semana 24 de julio 2015

--- js/mypassion.js
+++ js/mypassion.js
@@ -5,9 +5,10 @@ visor = $("#reloj"); //localizar pantalla del reloj
 //Botón Iniciar: Estado empezar: Poner en marcha el crono
 function empezar(datos) {
     var tipoBoton = $(this).children(".nom-boton").html();
-    if(tipoBoton == "Iniciar"){
+    if(tipoBoton == '<i class="fa fa-play"></i> Iniciar'){
         $(".btn").removeClass("disabled");
-        $(this).children(".nom-boton").html("Terminar");
+        $(this).children(".nom-boton").html('<i class="fa fa-eject"></i> Terminar');
+        $(this).addClass("disabled");
         emp = new Date(); //fecha inicial (en el momento de pulsar)
         elcrono = setInterval(tiempo,10); //función del temporizador.
         marcha = 1; //indicamos que se ha puesto en marcha.
@@ -35,9 +36,18 @@ function tiempo() {
     if (cs < 10) {cs = "0" + cs;} 
     if (sg < 10) {sg = "0" + sg;} 
     if (mn < 10) {mn = "0" + mn;}
-    if (cs > 99) {cs = "00";} 
-    //llevar resultado al visor.         
-    visor.html(mn + ":" + sg + ":" + cs); 
+    if (cs > 99) {cs = "00";}
+    //validamos que el tiempo sea inferior a 20 minutos
+    if((mn < 20) && (sg < 60)){
+        //llevar resultado al visor.
+        visor.html(mn + ":" + sg + ":" + cs);
+    }
+    else{
+        pausa();
+        visor.html(mn + ":" + sg + ":00");
+        $(".btn").addClass("disabled");
+    }
+     
 }
 //parar el cronómetro
 function parar() { 
@@ -59,10 +69,10 @@ function pausa (datos){
     var boton = $(this).attr('id');
     var tiempo_partido = $("#reloj").html();
     if((boton === 'tiempo-local') || (boton === 'tiempo-visita')){
-        $(this).children(".nom-boton").append("<br>" + tiempo_partido);
+        $(this).children(".hidden").append(tiempo_partido);
         $(this).addClass('disabled');
     }
-    if((boton === 'tarjeta-local') || (boton === 'tarjeta-visita') || (boton === 'falta-local') || (boton === 'falta-visita') || (boton === 'gol-local') || (boton === 'gol-visita')){
+    if((boton === 'tarjeta-local') || (boton === 'tarjeta-visita')){
         var botonTemp = boton.split('-');
         var botonShort = botonTemp[1];
         if(botonShort === 'local'){
@@ -75,15 +85,43 @@ function pausa (datos){
         $("#espere").load("./tarjetas.php?boton="+boton+"&equipo="+encodeURI(equipo));
         marcha = 1;
     }
+    if((boton === 'falta-local') || (boton === 'falta-visita')){
+        var botonTemp = boton.split('-');
+        var botonShort = botonTemp[1];
+        if(botonShort === 'local'){
+            var equipo = $("#equipo-local").html();
+        }
+        else{
+            var equipo = $("#equipo-visita").html();
+        }
+        $("#espere").removeClass("hidden");
+        $("#espere").load("./faltas.php?boton="+boton+"&equipo="+encodeURI(equipo));
+        marcha = 1;
+    }
+    if((boton === 'gol-local') || (boton === 'gol-visita')){
+        var botonTemp = boton.split('-');
+        var botonShort = botonTemp[1];
+        if(botonShort === 'local'){
+            var equipo = $("#equipo-local").html();
+        }
+        else{
+            var equipo = $("#equipo-visita").html();
+        }
+        $("#espere").removeClass("hidden");
+        $("#espere").load("./goles.php?boton="+boton+"&equipo="+encodeURI(equipo));
+        marcha = 1;
+    }
 
     var botonDetener = $("#detener-tiempo").children(".nom-boton").html();
     // Si la variable marcha es igual a 0 es por que el crono esta detenido 
     if (marcha == 0) { //con el boton en "continuar"
-        $("#detener-tiempo").children(".nom-boton").html("Detener");
+        $("#detener-tiempo").children(".nom-boton").html('<i class="fa fa-pause"></i> Detener');
+        $("#terminar-partido").addClass("disabled");
         continuar(); //ir a la función continuar
     }
     else {  //con el botón en "Detener"
-        $("#detener-tiempo").children(".nom-boton").html("Continuar");
+        $("#detener-tiempo").children(".nom-boton").html('<i class="fa fa-play"></i> Continuar');
+        $("#terminar-partido").removeClass("disabled");
         parar(); //ir a la funcion parar
     }
 }
@@ -96,7 +134,7 @@ function reiniciar() {
     //en cualquier caso volvemos a los valores iniciales
     cro=0; //tiempo transcurrido a cero
     visor.html ("00:00:00"); //visor a cero
-    $("#terminar-partido").children(".nom-boton").html("Iniciar");
+    $("#terminar-partido").children(".nom-boton").html('<i class="fa fa-play"></i> Iniciar');
     $("#terminar-partido").removeClass("disabled");
 }
 //Funcion que escucha los botones del archivo index.html al cargar el documento
@@ -116,7 +154,7 @@ function iniciar () {
 }
 // Funcion que se ejecuta al cargar el detalle del partido Tarjetas, Faltas y Goles
 function cargaDetalle () {
-    $(".col-lg-3 > .btn").on("click", agregarDetalle);
+    $(".tipo_tarjeta").on("click", cargaTarjeta);
 }
 // Funcion que se ejecuta al hacer click en los botones del detalle del partido en los numeros de los jugadores registrados
 function agregarDetalle (datos) {
@@ -125,11 +163,17 @@ function agregarDetalle (datos) {
         text: "Here's my error message!",
         type: "error",
         confirmButtonText: "Cool"
+    },
+    function () {
+        $("#espere").html("");
+        $("#espere").addClass("hidden");//Oculta el div del detalle
     });
-    $("#espere").html("");
-    $("#espere").addClass("hidden");//Oculta el div del detalle
-    $("#detener-tiempo").children(".nom-boton").html("Detener");
-    continuar(); //continua con el crono
+}
+// Funcion que se ejecuta al seleccionar el tipo de tarjeta que se va a imponer
+function cargaTarjeta (datos) {
+    var tipo = $(this).attr("name");
+    $(".col-lg-3 > .btn").removeClass("disabled");
+    $(".col-lg-3 > .btn").on("click", agregarDetalle);
 }
 
 
--- tarjetas.php
+++ tarjetas.php
@@ -8,89 +8,100 @@ if(isset($_GET['boton'])){
 if(isset($_GET['equipo'])){
 	$equipo = $_GET['equipo'];
 }
-if($boton == "tarjeta"){
-	$class = "warning";
-}
-elseif($boton == "falta"){
-	$class = "danger";
-}
-elseif($boton == "gol"){
-	$class = "info";
-}
 ?>
 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 	<?php echo $equipo;?>
-</div>
+</div>	
 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
-		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-<?php echo $class;?>">
+		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-default disabled">
 			<div class="clearfix nom-boton">1</div>
 		</div>
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">&nbsp;</div>
 	</div>
 	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
-		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-<?php echo $class;?>">
+		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-default disabled">
 			<div class="clearfix nom-boton">2</div>
 		</div>
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">&nbsp;</div>
 	</div>
 	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
-		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-<?php echo $class;?>">
+		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-default disabled">
 			<div class="clearfix nom-boton">3</div>
 		</div>
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">&nbsp;</div>
 	</div>
 	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
-		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-<?php echo $class;?>">
+		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-default disabled">
 			<div class="clearfix nom-boton">4</div>
 		</div>
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">&nbsp;</div>
 	</div>
 	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
-		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-<?php echo $class;?>">
+		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-default disabled">
 			<div class="clearfix nom-boton">5</div>
 		</div>
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">&nbsp;</div>
 	</div>
 	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
-		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-<?php echo $class;?>">
+		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-default disabled">
 			<div class="clearfix nom-boton">6</div>
 		</div>
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">&nbsp;</div>
 	</div>
 	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
-		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-<?php echo $class;?>">
+		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-default disabled">
 			<div class="clearfix nom-boton">7</div>
 		</div>
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">&nbsp;</div>
 	</div>
 	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
-		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-<?php echo $class;?>">
+		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-default disabled">
 			<div class="clearfix nom-boton">8</div>
 		</div>
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">&nbsp;</div>
 	</div>
 	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
-		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-<?php echo $class;?>">
+		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-default disabled">
 			<div class="clearfix nom-boton">9</div>
 		</div>
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">&nbsp;</div>
 	</div>
 	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
-		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-<?php echo $class;?>">
+		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-default disabled">
 			<div class="clearfix nom-boton">10</div>
 		</div>
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">&nbsp;</div>
 	</div>
 	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
-		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-<?php echo $class;?>">
+		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-default disabled">
 			<div class="clearfix nom-boton">11</div>
 		</div>
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">&nbsp;</div>
 	</div>
 	<div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
-		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-<?php echo $class;?>">
+		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 btn btn-default disabled">
 			<div class="clearfix nom-boton">12</div>
+		</div>
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">&nbsp;</div>
+	</div>
+</div>
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+	<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+		<div name="openid" class="col-xs-10 col-sm-10 col-md-10 col-lg-10 tipo_tarjeta btn btn-openid">
+			<div class="clearfix nom-boton"><i class="fa fa-tablet"></i>&nbsp;Amarilla</div>
+		</div>
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">&nbsp;</div>
+	</div>
+	<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+		<div name="dropbox" class="col-xs-10 col-sm-10 col-md-10 col-lg-10 tipo_tarjeta btn btn-dropbox">
+			<div class="clearfix nom-boton"><i class="fa fa-tablet"></i>&nbsp;Azul</div>
+		</div>
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">&nbsp;</div>
+	</div>
+	<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+		<div name="pinterest" class="col-xs-10 col-sm-10 col-md-10 col-lg-10 tipo_tarjeta btn btn-pinterest">
+			<div class="clearfix nom-boton"><i class="fa fa-tablet"></i>&nbsp;Roja</div>
 		</div>
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">&nbsp;</div>
 	</div>
